10a3c14aa945583c8bf29c3fb11a9a86210fe9f7
lipcha <vasia_lipcha@mail.ru>
2015-09-08 11:03:45 +0300

    Add SelectCurrentGeoLocationActivity

---
+++ app/src/main/java/com/mobstar/geo_filtring/SelectCurrentGeoLocationActivity.java
@@ -0,0 +1,23 @@
+package com.mobstar.geo_filtring;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.mobstar.R;
+
+/**
+ * Created by lipcha on 08.09.15.
+ */
+public class SelectCurrentGeoLocationActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_current_location);
+
+    }
+
+    private void findViews(){
+
+    }
+}
